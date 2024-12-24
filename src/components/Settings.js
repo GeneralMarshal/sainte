0dc723fd4e28f5ea1d54db2a5817ba5ab34d2bda
GeneralMarshal <112048946+GeneralMarshal@users.noreply.github.com>
2024-12-24 04:03:08 +0100

    ensuring babel is in devDependencies

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,7 +1,20 @@
 export default function Settings(){
     return(
-        <>
-            
-        </>
+       <main className=" w-full">
+            <section>
+                <div>
+                    <p>personal infor</p>
+                    <p>update your photo and personal detiails</p>
+                </div>
+                <div>
+                    <form action="">
+                        
+                    </form>
+                </div>
+            </section>
+            <section>
+
+            </section>
+       </main>
     )
 }
